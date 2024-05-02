7711c54537ef942f281277c464ffbc7575e09e25
MayaBO007 <77677870+MayaBO007@users.noreply.github.com>
2024-05-02 18:06:29 +0300

    ===

--- dayOrganizer.js
+++ dayOrganizer.js
@@ -4,28 +4,28 @@ function moveToDay() {
         getIndexSessionData(data).then((indexI) => {
             studySessionData = data[indexI];
 
-            if ((typeof studySessionData == "undefined") || (studySessionData.doneInstructions == "")) {
+            if ((typeof studySessionData === "undefined") || (studySessionData.doneInstructions === "")) {
                 platform.goToUrl("instructions/instructions.html");
             } else if (studySessionData.doneDay1 != "doneDayOne") {
-                if (studySessionData.doneDay1 == "") {
+                if (studySessionData.doneDay1 === "") {
                     platform.goToUrl("days/dayOne/training.html");
                 } else {
                     problemOrient()
                 }
             } else if (extractDeviceName(userAgent) == studySessionData.userDevice) {
-                if ((studySessionData.doneDay1 == "doneDayOne") && (studySessionData.doneDay2 != "doneDayTwo")) {
-                    if (studySessionData.doneDay2 == "") {
+                if ((studySessionData.doneDay1 === "doneDayOne") && (studySessionData.doneDay2 != "doneDayTwo")) {
+                    if (studySessionData.doneDay2 === "") {
                         platform.goToUrl("days/dayTwo/dayTwo.html");
                     } else {
                         problemOrient()
                     }
-                } else if ((studySessionData.doneDay2 == "doneDayTwo") && (studySessionData.doneDay3 != "doneDevTest")) {
-                    if (studySessionData.doneDay3 == "") {
+                } else if ((studySessionData.doneDay2 === "doneDayTwo") && (studySessionData.doneDay3 != "doneDevTest")) {
+                    if (studySessionData.doneDay3 === "") {
                         platform.goToUrl("days/dayThree/dayThree.html");
                     } else {
                         problemOrient()
                     }
-                } else if (studySessionData.doneDay3 == "doneDevTest") {
+                } else if (studySessionData.doneDay3 === "doneDevTest") {
                     endGameOrient()
                 } else {
                     problemOrient()
